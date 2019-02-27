ba69c089cf3121ea1be4068739f79fec8c773de7
Stephen Schulte <sschulte@ucvts.org>
2019-02-27 12:14:15 -0500

    changed to tkinter

--- guiCalc.py
+++ guiCalc.py
@@ -1,9 +1,6 @@
-import gi
-from _blueman import destroy_bridge
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from tkinter import *
 
-window = Gtk.Window(title="GUI Calculator")
-window.show()
-window.connect("destroy", Gtk.main_quit)
-Gtk.main()
+def calc(source, side):
+    storeObj = Frame(source, borderwidth=4, bd=4, bg="")
+    storeObj.pack(side=side, expand =YES, fill =BOTH)
+    return storeObj
